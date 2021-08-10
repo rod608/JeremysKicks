d827838f1b7bec281e674d099aa8ea3753e70a2e
rod608 <rod608ney@gmail.com>
2021-08-09 22:38:50 -0400

    Added responsive navbar toggler icon. Changed the color scheme and will modify again later based on said change.

---
+++ main.js
@@ -0,0 +1,12 @@
+const menuToggle = document.querySelector(".navbar-toggler");
+const menuBtn = document.querySelector("#menu-btn");
+let menuOpen = false;
+menuToggle.addEventListener("click", () => {
+    if(!menuOpen) {
+        menuBtn.classList.add("open");
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove("open");
+        menuOpen = false;
+    }
+});
